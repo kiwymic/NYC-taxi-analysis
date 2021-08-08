8f268cca1b499cdd02b840071918b58665840218
kiwymic <kiwymic@gmail.com>
2021-08-08 12:17:18 -0400

    Comleted a sloooooooooow function using for loop to go through a data frame and do the zone mapping thing. Takes 0.3-0.4s per entry.

--- NYC_taxicab/leaflet_test.R
+++ NYC_taxicab/leaflet_test.R
@@ -44,21 +44,80 @@ data_cleaning = function(df){
 }
 
 data_processing = function(df){
-  # Restructure the date/time
+  # Here, we collect processing functions which processes date-time
+  # information.
   df <- df %>% mutate(tpep_pickup_datetime = ymd_hms(tpep_pickup_datetime),
                     tpep_dropoff_datetime = ymd_hms(tpep_dropoff_datetime),
                     duration = tpep_dropoff_datetime - tpep_pickup_datetime,
-                    speed = trip_distance/as.numeric(duration))
-  # Add duration.
+                    speed = trip_distance/as.numeric(duration),
+                    in_pocket = fare_amount + extra + tip_amount)
   return(df)
 }
 
+zone_lookup = function(st){
+  return(match(1,st_contains(ny_areas, st)))
+}
+
+distance_processing = function(df){
+  # Here, we collect processing functions which processes geospacial
+  # information. Mainly, the zone info and distance info.
+  
+  # Adding the zone information. 
+  # For this to be functioned properly, one must make sure that
+  # ./NYC_taxicab/taxi_zones.shp has been read as ny_areas and processed.
+  df <- df %>%
+    mutate(pickup_sf = st_point(as.numeric(c(pickup_longitude,
+                                             pickup_latitude))),
+           dropoff_sf = st_point(as.numeric(c(dropoff_longitude,
+                                             dropoff_latitude))),
+           pickup_zone = match(1,st_contains(ny_areas, pickup_sf)),
+           dropoff_zone = match(1,st_contains(ny_areas, dropoff_sf)))
+  
+  df <- df %>%
+    mutate(earth_distance =
+             as.numeric(st_length(st_sfc(st_linestring(
+               rbind(pickup_sf, dropoff_sf), crs = 4326)))))
+}
+
+# A bad non-vectorized method to do all the zone mappings.
+# Should be slow. Should be replaced
+distance_processing_nv = function(df){
+  df_patch = read.table(text = "",
+                        col.names = c("pickup_zone", "dropoff_zone",
+                                      "earth_distance"))
+  
+  for(i in 1:dim(df)[1]){
+    pickup_sf = st_point(as.numeric(c(df$pickup_longitude[i],
+                                      df$pickup_latitude[i])))
+    dropoff_sf = st_point(as.numeric(c(df$dropoff_longitude[i],
+                                       df$dropoff_latitude[i])))
+    pickup_zone = match(1,st_contains(ny_areas, pickup_sf))
+    dropoff_zone = match(1,st_contains(ny_areas, dropoff_sf))
+    
+    earth_distance = st_length(st_sfc(st_linestring(
+      rbind(pickup_sf, dropoff_sf)), crs = 4326))
+    
+    df_patch = df_patch %>% add_row(pickup_zone = pickup_zone,
+                            dropoff_zone = dropoff_zone,
+                            earth_distance =
+                              as.numeric(earth_distance)*0.000621371192)
+  }
+  return(cbind(df, df_patch))
+}
+# A few noticeable zones:
+# Airport: EWR = 1, JFK = 132, LGA = 138
+
+temp = trip[10001:20000,]
+ptm <- proc.time()
+temp = distance_processing_nv(temp)
+proc.time() - ptm
+# 0.3-0.4s per entry... desperate
+
+# write.csv(temp %>% select(-1), "y1_00001-10000.csv", row.names = T)
+write.csv(temp , "y1_00001-10000_cp.csv", row.names = T)
+
 # Loading the ride data
 trip = read.csv("./NYC_taxicab/y1.csv")
-
-# You must process before you clean. Otherwise, you die.
-trip = data_processing(trip)
-trip = data_cleaning(trip)
 
 # Loading the zone map from NYC. The projection in that file is in
 # EPSG:2908 (whatever it is).
@@ -66,6 +125,11 @@ trip = data_cleaning(trip)
 # (your daily friendly lat/lon), so you need to translate it.
 ny_areas <- st_read("./NYC_taxicab/taxi_zones.shp") %>%
   st_transform(crs = 4326)
+
+# You must process before you clean. Otherwise, you die.
+trip = data_processing(trip)
+# trip = distance_processing(trip)
+trip = data_cleaning(trip)
 
 test.df <- trip %>% head(10000) %>%
   select(lng = pickup_longitude, lat = pickup_latitude) 
@@ -76,8 +140,8 @@ m <- leaflet(ny_areas) %>% addPolygons(color = "#004499", weight = 1,
       smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.3) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   setView( lng = -74.0, lat = 40.74, zoom = 10 ) %>%
-  addCircles(data = test.df, weight = 0, color = "red") #%>%
-  # addCircles(data = test2.df, weight = 0, color = "green")
+  # addCircles(data = test.df, weight = 0, color = "red") #%>%
+  addCircles(data = test2.df, weight = 0, color = "green")
   
 m
 
